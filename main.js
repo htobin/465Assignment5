4315b65e1a15e0418831db0196293627d92c4cd6
htobin <htobin@hawaii.edu>
2020-03-02 01:56:28 -1000

    Comments for clarity

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
+//main function
 $(function (){
+  //copy to clipboard from textbox 1
   $('#text1').bind('copy', function() {
     highlight($(this)[0]);
   });
+  //copy to clipboard from textbox 2
   $('#text2').bind('copy', function() {
     highlight($(this)[0]);
   });
-  $('#text1').bind('paste', function() {
-    paste($(this)[0]);
+  
+  //
+  // $('#text1').bind('paste', function() {
+  //   paste($(this)[0]);
+  // });
+  // $('#text2').bind('paste', function() {
+  //   paste($(this)[0]);
+  // });
+
+  //paste to textbox 1 with PASTE button
+  $('#paste').on('click',function(){
+    paste($('#text1'));
   });
-  $('#text2').bind('paste', function() {
-    paste($(this)[0]);
-  });
-
-
-
+  //paste to textbox 2 with PASTE button
   $('#paste').on('click',function(){
     paste($('#text2'));
-  })
+  });
+
+  //function that highlights the text that will be pasted to clipboard
+  //parameter: currentText, text that will be highlighted
   function highlight(currentText) {
     $('#clipboard').text("");
     var start = currentText.selectionStart;
@@ -26,6 +37,8 @@ $(function (){
     $('#clipboard').text(selection);
   }
 
+  //function that pastes from ta cliboard to textbox
+  //parameter:textBox, the target textbox that will be pasted in
   function paste (textBox){
     var currentText = $('#clipboard').text();
     var start = textBox[0].selectionStart;
