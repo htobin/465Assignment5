dfa57b250a0f9da2fb92a333738267b6cb848d3e
Alton Lee <altonlee@hawaii.edu>
2020-03-11 08:57:04 -1000

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ var clip = [];
 var startForCut = 0;
 var endForCut = 0;
 var activeTextBox = null;
+var cutFromThisBox = null;
+
+/*
+ * BUTTON FUNCTIONS
+*/
 
 // function that copies clipboard into current active textbox
 function copy() {
     var clipboard = document.getElementById('clipboard').innerHTML;
     
     if (activeTextBox.id === 'text1') {
-        /* do paste */
         paste("text1", clipboard);
     } else {
-        /* do paste */
         paste("text2", clipboard);
     }
 }
@@ -23,55 +26,91 @@ function cut() {
     var clipboard = document.getElementById('clipboard').innerHTML;
     
     if (activeTextBox.id === 'text1') {
-        /* do paste */
-        paste("text1", clipboard);
-        document.getElementById('text2').innerHTML.replace(clipboard, '');
+        paste("text1", clipboard, startForCut, endForCut);
+        paste(cutFromThisBox.id, "", cutFromThisBox.selectionStart, cutFromThisBox.selectionEnd);
     } else {
-        /* do paste */
-        paste("text2", clipboard);
-        document.getElementById('text1').innerHTML.replace(clipboard, '');
+        paste("text2", clipboard, startForCut, endForCut);
+        paste(cutFromThisBox.id, "", cutFromThisBox.selectionStart, cutFromThisBox.selectionEnd);
     }
 }
 
-// function handles string manipulation for pasting clipboard
+/*
+ * KEYBOARD FUNCTIONS
+ * Credit: Hoku Tobin
+*/
+
+  //take copy from clipboard to textbox1.
+  $('#text1').bind('copy', function(e) {
+    e.preventDefault();
+    var clipboard = document.getElementById('clipboard').innerHTML;
+    paste("text1", clipboard);
+  });
+
+  //take copy from clipboard to textbox2.
+  $('#text2').bind('copy', function(e) {
+    e.preventDefault();
+    var clipboard = document.getElementById('clipboard').innerHTML;
+    paste("text2", clipboard);
+  });
+
+  //take cut from clipboard to textbox1.
+  $('#text1').bind('cut', function(e) {
+    e.preventDefault();
+    var clipboard = document.getElementById('clipboard').innerHTML;
+    paste("text1", clipboard, startForCut, endForCut);
+    paste(cutFromThisBox.id, "", cutFromThisBox.selectionStart, cutFromThisBox.selectionEnd);
+  });
+
+  //take cut from clipboard to textbox2.
+  $('#text2').bind('cut', function(e) {
+    e.preventDefault();
+    var clipboard = document.getElementById('clipboard').innerHTML;
+    paste("text2", clipboard, startForCut, endForCut);
+    paste(cutFromThisBox.id, "", cutFromThisBox.selectionStart, cutFromThisBox.selectionEnd);
+  });
+  
+/*
+ * OTHER FUNCTIONS
+*/
+
+// function handles string manipulation for pasting clipboard.
 // credit: Hoku Tobin
-function paste(dest, clip) {
+function paste(dest, clip, cutStart, cutEnd) {
     var box = document.getElementById(dest);
-    var firstChunk = box.innerHTML.substring(0,startForCut);
-    var endChunk = box.innerHTML.substring(endForCut);
+    var firstChunk = box.innerHTML.substring(0,cutStart);
+    var endChunk = box.innerHTML.substring(cutEnd);
     box.innerHTML = firstChunk + clip + endChunk;
 }
 
+// function that updates clipboard history.
 function clipHistory() {
     var ret = "Clipboard History:\n<ol>";
     
     for (var i = 0; i < clip.length; i++) {
-        console.log(clip[i]);
         ret += "<li>" + clip[i] + "</li>";
     }
     ret += "</ol>\n";
-    console.log(ret);
     document.getElementById('clip-history').innerHTML = ret;
 }
 
 /* 
- * copies selection from either textbox and saves it into clipboard
+ * copies selection from either textbox and saves it into clipboard.
  * code inspired from: 
  * https://developer.mozilla.org/en-US/docs/Web/API/DocumentOrShadowRoot/activeElement
 */
 function onMouseUp(e) {
     activeTextBox = document.activeElement;
-    console.log(activeTextBox);
     var selection;
 
     selection = activeTextBox.value.substring(
         activeTextBox.selectionStart, activeTextBox.selectionEnd
     );
-
+    
     startForCut = activeTextBox.selectionStart;
     endForCut = activeTextBox.selectionEnd;
 
     if (selection != "") {
+      cutFromThisBox = activeTextBox;
       document.getElementById('clipboard').innerHTML = selection;
       document.getElementById('clipboard').value = selection;
       clip.push(selection);
